41113efdd58bb70568215b82f82431e747f2dd3b
Gaizka Valencia <31756055+gvlencia@users.noreply.github.com>
2020-11-23 13:37:43 +0700

    Django user roles

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from .models import ProgramStudi, User, Account
 # Register your models here.
+
+@admin.register(User)
+class UserModelAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields' : ('email', 'username', 'password')}),
+        ('Personal Detail', {'fields' : ('first_name', 'last_name', 'prodi', 'roles')}),
+        ('Permissions', {'fields' : ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields' : ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields' : ('username', 'email', 'password1', 'password2', 'prodi', 'roles')
+        })
+    )
+
+admin.site.register(ProgramStudi)
+admin.site.register(Account)
--- account/models.py
+++ account/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, UserManager
 
 # Create your models here.
+class ProgramStudi(models.Model):
+    nama_prodi = models.CharField(max_length=200)
+    def __str__(self):
+        return self.nama_prodi
+
+class Account(models.Model):
+    nama_akun = models.CharField(max_length=200)
+    prodi = models.ForeignKey(ProgramStudi, models.SET_NULL, blank=True, null=True, default=None)
+
+
+class User(AbstractUser):
+    prodi = models.ForeignKey(ProgramStudi, models.SET_NULL, blank=True, null=True, default=None)
+    role_list = (
+        ('Admin', 'Admin'),
+        ('Faculty Member', 'Faculty Member'),
+        ('Professional Staf', 'Professional Staf')
+    )
+    roles = models.CharField(choices=role_list, max_length=40)
+
+    objects = UserManager
