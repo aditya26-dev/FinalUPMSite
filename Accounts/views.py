0f8dd5a1ee5bdcfcfa082b1c82c47999386b2b71
Gaizka Valencia <31756055+gvlencia@users.noreply.github.com>
2020-11-25 19:33:22 +0700

    Recovery Password

--- Accounts/views.py
+++ Accounts/views.py
@@ -1,4 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from .decorators import unauthenticated_user
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@unauthenticated_user
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else :
+            messages.info(request, 'Email or Password is incorrect')
+
+    return render(request, 'UPM_Login.html')
+
+def logoutUser(request):
+    logout(request)
+    return redirect("login")
+
+@login_required(login_url='login')
+def home(request):
+    return render(request, 'main.html')
+
 
