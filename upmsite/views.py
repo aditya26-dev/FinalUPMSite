fdbc40b2205abee8a102368f043de3fdc955aa46
Software Engineering Prasmul <softwareengineeringprasmul@Softwares-MacBook-Pro.local>
2020-11-27 09:35:50 +0700

    change home views Account to upmsite

--- upmsite/views.py
+++ upmsite/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
-def loginPage(request):
-    return render (request,'UPM_Login.html')
+@login_required(login_url='login')
+def home(request):
+    # query ke model Folder based on is_public = True , Category informasi umum 
+    # kemudian pass konten ke main.html 
+
+    return render(request, 'main.html')
