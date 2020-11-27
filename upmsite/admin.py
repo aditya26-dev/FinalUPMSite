aa1a2e094d726b03908eb706bce0c8bf38cd6c84
aditya26-dev <dewaa.kadek26@gmail.com>
2020-11-27 10:31:32 +0800

    update adding models

--- upmsite/admin.py
+++ upmsite/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . import models
 
+admin.site.site_header = "DJANGO ADMIN UPM SITE"
+admin.site.register(models.Prodi)
+admin.site.register(models.Folder)
+admin.site.register(models.File)
+admin.site.register(models.SubFolder01)
+admin.site.register(models.SubFolder02)
+admin.site.register(models.SubFile01)
+admin.site.register(models.SubFile02)
 # Register your models here.
