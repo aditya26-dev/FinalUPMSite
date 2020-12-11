c0f79cb10f31a84cca3713880263ec6108b5f77c
Software Engineering Prasmul <softwareengineeringprasmul@Softwares-MacBook-Pro.local>
2020-12-11 13:32:58 +0700

    add widgets CreateUser

--- Accounts/forms.py
+++ Accounts/forms.py
@@ -1,23 +1,55 @@
 from .models import *
 from django.forms import ModelForm
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
 # from django.contrib.auth.models import User
 from Accounts.models import User 
 
-class CreationUserForm(ModelForm):
-    class Meta:
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+# from .models import CustomUser
+
+
+class CreationUserForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super(CreationUserForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
+
+    class Meta(UserCreationForm):
         model = User
-        fields = '__all__'
-        print(fields)
-        # fields = ('username', 'email', 'password1', 'password2', 'prodi', 'roles')
-        
+        fields = ('username', 'email', 'password1', 'password2', 'prodi', 'roles')
         widgets = {
-            'username' : forms.TextInput(attrs={'class': 'input', 'placeholder' : 'username'}),
-            'email' : forms.EmailInput(attrs={'class': 'input', 'placeholder' : 'email'}),
-            # 'password1' : forms.PasswordInput(attrs={'class': 'input', 'placeholder' : 'password'}),
-            # 'password2' : forms.PasswordInput(attrs={'class': 'input', 'placeholder' : 'password'}),
+            'username' : forms.TextInput(attrs={'class': 'form-control', 'placeholder' : 'username'}),
+            'email' : forms.EmailInput(attrs={'class': 'form-control', 'placeholder' : 'email'}),
+            'password1' : forms.PasswordInput(attrs={'class': 'input', 'placeholder' : 'password'}),
+            'password2' : forms.PasswordInput(attrs={'class': 'input', 'placeholder' : 'password'}),
             'prodi' : forms.Select(attrs={'class': 'form-control'}),
             'roles' : forms.Select(attrs={'class': 'form-control'}),
-            # 'profile_picture' : forms.ImageField(),
+            'profile_picture' : forms.ImageField(),
         }
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = User
+        fields = ('email',)
+
+# class CreationUserForm(ModelForm):
+#     class Meta:
+#         model = User
+#         fields = '__all__'
+#         print(fields)
+#         # fields = ('username', 'email', 'password1', 'password2', 'prodi', 'roles')
+        
+#         widgets = {
+#             'username' : forms.TextInput(attrs={'class': 'input', 'placeholder' : 'username'}),
+#             'email' : forms.EmailInput(attrs={'class': 'input', 'placeholder' : 'email'}),
+#             # 'password1' : forms.PasswordInput(attrs={'class': 'input', 'placeholder' : 'password'}),
+#             # 'password2' : forms.PasswordInput(attrs={'class': 'input', 'placeholder' : 'password'}),
+#             'prodi' : forms.Select(attrs={'class': 'form-control'}),
+#             'roles' : forms.Select(attrs={'class': 'form-control'}),
+#             # 'profile_picture' : forms.ImageField(),
+#         }
